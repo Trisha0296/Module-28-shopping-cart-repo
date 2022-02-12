4199cbf5a1d0b662f1d76a53ae105c77f267c4c7
Trisha0296 <trishah0296@gmail.com>
2022-02-13 01:57:16 +0600

    this is second commit

--- app-srot.js
+++ app-srot.js
@@ -1,18 +1,66 @@
 // 01 ful-case
-function updateCaseNumber(isIncreasing) {
-  const caseInput = document.getElementById('case-number');
-  const caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing) {
+  const productInput = document.getElementById(product + '-number');
+  let productNumber = productInput.value;
   if (isIncreasing == true) {
-    caseInput.value = parseInt(caseNumber) + 1;
-  } else if (caseNumber > 0) {
-    caseInput.value = parseInt(caseNumber) - 1;
+    productNumber = parseInt(productNumber) + 1;
+  } else if (productNumber > 0) {
+    productNumber = parseInt(productNumber) - 1;
   }
+  productInput.value = productNumber;
+  //update  total
+  const ProductTotal = document.getElementById(product + '-total');
+  ProductTotal.innerText = productNumber * price;
+  // calculate Total
+  calculateTotal();
 }
+
+// phoner increase decrease events----
+/* phone-plus */
+document.getElementById("phone-plus").addEventListener('click', function () {
+  updateProductNumber('phone', 1219, true);
+});
+/* phone-minus*/
+document.getElementById('phone-minus').addEventListener('click', function () {
+  updateProductNumber('phone', 1219, false);
+})
+// handle case increase decrease events----
 /* cese-plus */
 document.getElementById('cese-plus').addEventListener('click', function () {
-  updateCaseNumber(true);
+  updateProductNumber('case', 59, true);
 });
-/* case-minus */
+// /* case-minus */
 document.getElementById('case-minus').addEventListener('click', function () {
-  updateCaseNumber(false);
+  updateProductNumber('case', 59, false);
 })
+
+/* 02 */
+/* second-part */
+function getInputValu(product) {
+  const productInput = document.getElementById(product + '-number')
+  const productNumber = parseInt(productInput.value);
+  return productNumber;
+}
+/* 01 */
+// total-price
+function calculateTotal() {
+  const phoneTotal = getInputValu('phone') * 1219;
+  const caseTotal = getInputValu('case') * 59;
+  const subTotal = phoneTotal + caseTotal;
+  // update tax
+  const taxAmount = subTotal / 10;
+  // total amount
+  const totalPrice = subTotal + taxAmount;
+  // update on the html
+  document.getElementById('sub-total').innerText = subTotal;
+  document.getElementById('tax-amount').innerText = taxAmount;
+  document.getElementById('total-price').innerText = totalPrice;
+}
+
+
+
+// function calculateTotal() {
+//   const phoneInput = document.getElementById('phone-number')
+//   const phoneNumber = parseInt(phoneInput.value);
+//   const phoneTotal = phoneNumber * 1219;
+// }
--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 // console.log('I am in a separate file')
 
-/* 01 */
+/* 01-case*/
 document.getElementById('cese-plus').addEventListener('click', function () {
   /*   02 */
   const caseInput = document.getElementById('case-number');
-  const caseNumber = caseInput.value;
-  caseInput.value = parseInt(caseNumber) + 1;
+  let caseNumber = caseInput.value;
+  caseInput.value = caseNumber;
+  caseNumber = parseInt(caseNumber) + 1;
+  //update case total
+  /* 04 */
+  const caseTotal = document.getElementById('case-total');
+  caseTotal.innerText = caseNumber * 59
+  caseInput.value = caseNumber;
 });
 /* 03 */
 document.getElementById('case-minus').addEventListener('click', function () {
   const caseInput = document.getElementById('case-number');
-  const caseNumber = caseInput.value;
-  caseInput.value = parseInt(caseNumber) - 1;
+  let caseNumber = caseInput.value;
+  caseNumber = parseInt(caseNumber) - 1;
+  //update case total
+  /* 04 */
+  const caseTotal = document.getElementById('case-total');
+  caseTotal.innerText = caseNumber * 59
+  caseInput.value = caseNumber;
+})
+// phoner increase decrease events----
+/* 01-phoner */
+/* plus */
+document.getElementById("phone-plus").addEventListener('click', function () {
+  const phoneInput = document.getElementById('phone-number');
+  let phoneNumber = phoneInput.value;
+  phoneNumber = parseInt(phoneNumber) + 1;
+  //update phone total
+  const phoneTotal = document.getElementById('phone-total');
+  phoneTotal.innerText = phoneNumber * 1219;
+  phoneInput.value = phoneNumber;
+})
+/*  minus*/
+document.getElementById("phone-minus").addEventListener('click', function () {
+  const phoneInput = document.getElementById('phone-number');
+  let phoneNumber = phoneInput.value;
+  phoneNumber = parseInt(phoneNumber) - 1;
+  //update phone total
+  const phoneTotal = document.getElementById('phone-total');
+  phoneTotal.innerText = phoneNumber * 1219;
+  phoneInput.value = phoneNumber;
+  console.log(phoneNumber);
 })
